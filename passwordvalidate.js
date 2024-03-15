9c303bd44b78b3a861eeda8654a8d9d85c73475a
tapasya v <typecode57@gmail.com>
2024-03-15 15:28:30 +0530

    s3

---
+++ passwordvalidate.js
@@ -0,0 +1,6 @@
+function isStrongPassword(password) {
+    const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return strongPasswordRegex.test(password);
+}
+
+module.exports = isStrongPassword;
